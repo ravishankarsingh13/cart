562dc94b7f9aac5f1ec0877641152927678a4ace
Ravi Shankar Singh <ravi13eng@gmail.com>
2020-07-08 22:41:40 +0530

    prop  added

--- src/Cart.js
+++ src/Cart.js
@@ -2,12 +2,42 @@ import React from 'react';
 import CartItem from './CartItem';
 
 class Cart extends React.Component{
+    constructor () {
+        super();
+        this.state = {
+          products: [
+              {
+                price: 99,
+                title: 'watch',
+                qty: 1,
+                img: '',
+                id:1
+              },
+              {
+                price: 999,
+                title: 'Mobile Phone',
+                qty: 1,
+                img: '',
+                id:2
+              },
+              {price: 999,
+                title: 'laptop',
+                qty: 1,
+                img: '',
+                id:3
+            }
+          ]
+        }
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+      }
     render(){
+        const {products} = this.state;
         return (
             <div className="cart">
-                <CartItem />
-                <CartItem />
-                <CartItem />
+                {products.map((product)=>{
+                    return <CartItem product ={product} key= {product.id}/>
+                })}
+                
             </div>
         );
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,16 +1,6 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-  constructor () {
-    super();
-    this.state = {
-      price: 999,
-      title: 'Mobile Phone',
-      qty: 1,
-      img: ''
-    }
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
-  }
   increaseQuantity = () => {
     console.log('this', this.state);
     // set state form 1
@@ -34,7 +24,8 @@ class CartItem extends React.Component {
     })
   }
   render () {
-    const { price, title, qty } = this.state;
+    console.log('this.props',this.props);
+    const { price, title, qty } = this.props.product;
     return (
       <div className="cart-item">
         <div className="left-block">
