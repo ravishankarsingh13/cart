606cd16207be261b5f3ce0dab925f0ed79e6b75e
Ravi Shankar Singh <ravi13eng@gmail.com>
2020-07-09 16:31:50 +0530

    added cart total

--- src/App.js
+++ src/App.js
@@ -11,20 +11,20 @@ class App extends React.Component {
             price: 99,
             title: 'watch',
             qty: 1,
-            img: '',
+            img: 'https://images.unsplash.com/photo-1542496658-e33a6d0d50f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
             id:1
           },
           {
             price: 999,
             title: 'Mobile Phone',
             qty: 1,
-            img: '',
+            img: 'https://images.unsplash.com/photo-1565849904461-04a58ad377e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=376&q=80',
             id:2
           },
-          {price: 999,
+          {price: 9999,
             title: 'laptop',
             qty: 1,
-            img: '',
+            img: 'https://images.unsplash.com/photo-1504707748692-419802cf939d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
             id:3
         }
       ]
@@ -75,6 +75,16 @@ class App extends React.Component {
     return count;
   }
 
+  getCartTotal = () =>{
+    const {products} = this.state;
+
+    let cartTotal = 0;
+
+    products.map((product) =>{
+      cartTotal = cartTotal + product.qty * product.price
+    })
+    return cartTotal;
+  }
 
   render(){
     const {products} = this.state;
@@ -87,6 +97,7 @@ class App extends React.Component {
         onDecreaseQuantity = {this.handleDecreaseQuantity}
         onDeleteProduct = {this.handleDeleteProduct}
         />
+        <div style={{padding:10,fontSize:20}} >Total: {this.getCartTotal()}</div>
       </div>
     );
   }
